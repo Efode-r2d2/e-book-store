e89607e4247a83dda040cb413031bcc0763e24fb
Efriem D. Gebie <efidesalew@gmail.com>
2022-05-22 17:16:39 +0300

    done with login layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,51 +9,52 @@
         <link rel="stylesheet" href="{{asset('fonts')}}">
     </head>
     <body class="hold-transition login-page">
-    <div class="login-box">
-    <div class="login-logo">
-        <h1>e<b>BookStore</b></h1>
-    </div>
+        <div class="login-box">
+            <div class="login-logo">
+                <h1>e<b>BookStore</b></h1>
+            </div>
 
-    <div class="card">
-    <div class="card-body login-card-body">
-    <p class="login-box-msg">Sign in to eBookStore</p>
-    <form action="../../index3.html" method="post">
-    <div class="input-group mb-3">
-    <input type="email" class="form-control" placeholder="Email">
-    <div class="input-group-append">
-    <div class="input-group-text">
-    <span class="fas fa-envelope"></span>
-    </div>
-    </div>
-    </div>
-    <div class="input-group mb-3">
-    <input type="password" class="form-control" placeholder="Password">
-    <div class="input-group-append">
-    <div class="input-group-text">
-    <span class="fas fa-lock"></span>
-    </div>
-    </div>
-    </div>
-    <div class="row">
-    <div class="col-8">
-
-    </div>
-
-    <div class="col-4">
-    <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-    </div>
-
-    </div>
-    </form>
-
-
-    <p class="mb-1">
-    <a href="forgot-password.html">I forgot my password</a>
-    </p>
-    </div>
-
-    </div>
-    </div>
-    <script src="{{asset('js/app.js')}}"></script>
+            <div class="card">
+                <div class="card-body login-card-body">
+                    <p class="login-box-msg">Sign in to eBookStore</p>
+                    <!-- form start here -->
+                    <form action="{{route('dashboard.index')}}" method="POST">
+                        @csrf
+                        <!-- email div starts here-->
+                        <div class="input-group mb-3">
+                            <input type="email" class="form-control" name="email" placeholder="Email">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- email div ends here-->
+                        <!-- password div starts here -->
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- password div ends here -->
+                        <!-- login button div starts here -->
+                        <div class="row">
+                            <div class="col-8">
+                            </div>
+                            <div class="col-4">
+                                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                            </div>
+                        </div>
+                        <!-- login button div ends here-->
+                    </form>
+                    <!-- form ends here -->
+                    <p class="mb-1"><a href="forgot-password.html">I forgot my password</a></p>
+                </div>
+            </div>
+        </div>
+        <script src="{{asset('js/app.js')}}"></script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+
+Route::post('/dashboard', function () {
+    return view('dashboard.index');
+})->name('dashboard.index');
